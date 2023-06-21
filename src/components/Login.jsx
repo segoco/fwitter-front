028a5d317d967dda92a32481cbf3a3aa3a77a0ae
Sergio Gomez C <segoco@gmail.com>
2023-06-21 13:42:53 -0500

    Added facebook and Gmail buttons to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,44 +1,49 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Login() {
   return (
     <div className='Login'>
-      <div class='container'>
-        <div class='div-1'>
-          <div class='sub-div-1'>
-          {/* <FontAwesomeIcon icon="fa-duotone fa-cat" /> */}
-          </div>
+      <div className='container'>
+        <div className='logoHome'>
+          <i className='fas fa-cat fa-2x'></i>
         </div>
+
         <form method='POST' action='http://google.com'>
-          <div class='div-2'>
-            <div class='sub-div-2'>
+          <div className='div-2'>
+            <div className='sub-div-2'>
               <h3>Log in to Fwitter</h3>
             </div>
           </div>
-          <div class='div-3'>
-            <div class='sub-div-3'>
-              <br />
-              <input type='email' placeholder='email' />
+          <div className='div-3'>
+            <div className='sub-div-3'>
+              <input type='email' placeholder='Your email (fi, user@something.co)' />
             </div>
           </div>
-          <div class='div-4'>
-            <div class='sub-div-4'>
-              <label>Password</label>
-              <br />
-              <input type='password' />
+          <div className='div-4'>
+            <div className='sub-div-4'>
+              <input type='password' placeholder='Your Password' />
             </div>
           </div>
-          <div class='div-5'>
-            <div class='sub-div-5'>
+          <div className='div-5'>
+            <div className='sub-div-5'>
               <button type='submit'>Log in</button>
             </div>
           </div>
         </form>
-        <div class='div-6'>
-          <div class='sub-div-6'>
+        <div className='div-6'>
+          <div className='sub-div-6'>
             <a href='#'>Forgot password? .&nbsp;</a>
-            <a href='#'>Sign up for Twitter</a>
+          </div>
+        </div>
+        <div className='div-7'>
+          <div className='sub-div-7'>
+            <h3>Sign up with Gmail or Facebook</h3>
+            <a href='#loginFacebook'>
+              <i className='fab fa-facebook-square fa-2x'></i>
+            </a>
+            <a href='#loginFacebook'>
+              <i className='fab fa-google fa-2x'></i>
+            </a>
           </div>
         </div>
       </div>
